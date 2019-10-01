8f683f62dc5cb2a2143ce7b72e0ad55ab034b157
vander0271 <vanderleonardo0271@gmail.com>
2019-09-30 21:38:08 -0300

    commit inicial

---
+++ app/src/main/java/br/com/senaigo/mobile/vouaprenderandroid/entidades/Instituição.java
@@ -0,0 +1,13 @@
+package br.com.senaigo.mobile.vouaprenderandroid.entidades;
+
+public class Instituição {
+
+    protected String nome;
+    protected String razaoSocial;
+    protected String cnpj;
+    protected String telefone;
+    protected String inscricaoEstadual;
+    protected String inscricaoMunicipal;
+    protected String endereco;
+
+}
